7a005d63b3d62cfda27c324455cb3b311941fb19
Ulad Birukou <alorine99@gmail.com>
2023-07-24 18:26:02 +0300

    feat: Init project

---
+++ .eslintrc.js
@@ -0,0 +1,61 @@
+module.exports = {
+  root: true,
+  settings: {
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  extends: 'airbnb',
+  plugins: [
+    'react',
+    'react-native',
+    'react-hooks',
+    'jsx-a11y',
+    'import',
+    'simple-import-sort',
+    'prettier',
+  ],
+  parser: '@babel/eslint-parser',
+  env: {
+    jest: true,
+    'react-native/react-native': true,
+  },
+  rules: {
+    'no-use-before-define': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/prop-types': 'off',
+    'comma-dangle': 'off',
+    'padded-blocks': 'off',
+    'arrow-body-style': 'off',
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'react-native/no-unused-styles': 2,
+    'react-native/split-platform-components': 2,
+    'react-native/no-inline-styles': 2,
+    'react-native/no-color-literals': 2,
+    'react-native/no-raw-text': 2,
+    'react-native/no-single-element-style-arrays': 2,
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'prettier/prettier': 'error',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never',
+        js: 'never',
+        jsx: 'never',
+        mjs: 'never',
+      },
+    ],
+  },
+  globals: {
+    fetch: false,
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 80,
+  userTabs: false,
+  tabWidth: 2,
+  semi: true,
+  arrowParens: 'avoid',
+  singleQuote: true,
+  endOfLine: 'lf',
+  trailingComma: 'all',
+};
