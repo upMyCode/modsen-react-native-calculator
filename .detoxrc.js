d6e04003fb3eb2565486bb3465a6a776f4e0e0a1
Ulad Birukou <alorine99@gmail.com>
2023-08-15 08:00:32 +0300

    Fix: Add config for android

--- .detoxrc.js
+++ .detoxrc.js
@@ -55,7 +55,8 @@ module.exports = {
     emulator: {
       type: 'android.emulator',
       device: {
-        avdName: 'Pixel_3a_API_30_x86',
+        avdName: 'Pixel_4_XL_API_30_2',
+        avdName: 'Pixel_4_XL_API_30',
       },
     },
   },
